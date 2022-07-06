fabaf4f8c1207590115d64e2600276a626f65f8b
swarnab99 <swarnab93@gmail.com>
2022-07-06 10:49:58 +0530

    services section updated

--- components/service/ServicesSection.js
+++ components/service/ServicesSection.js
@@ -3,9 +3,17 @@ import Link from 'next/link';
 import { RichText } from 'prismic-reactjs';
 import { CustomLink, DocLink } from '../../utils/prismicHelpers';
 
-const ServicesSection = ({ slice, show }) => {
+const ServicesSection = ({ slice }) => {
 	// console.log(slice);
-	const { heading, subheading } = slice?.primary;
+	const {
+		heading,
+		subheading,
+		diseases_col_1,
+		diseases_col_2,
+		diseases_col_3,
+		diseases_col_4,
+		diseases_col_5,
+	} = slice?.primary;
 	return (
 		<section className='wrapper pt-14 bg-light'>
 			<div className='container'>
@@ -31,85 +39,49 @@ const ServicesSection = ({ slice, show }) => {
 						))}
 					</div>
 				</div>
-				{show && (
+				{diseases_col_1 && (
 					<div className='mt-12'>
 						<div className='mt-4  '>
 							<h6 className='me-2'>Other Reversal Programs:</h6>{' '}
 							<div className='row'>
-								<div className='col-3'>
-									<BadgeItem title='Acid Reflux' color='red' />
-									<BadgeItem title='Acidity' color='red' />
-									<BadgeItem title='Acne' color='red' />
-									<BadgeItem title='Alzheimer' color='red' />
-									<BadgeItem title='Ankylosing Spondylitis' color='red' />
-									<BadgeItem title='Anxiety' color='red' />
-									<BadgeItem title='Arthritis' color='red' />
-									<BadgeItem title='Asthma' color='red' />
-									<BadgeItem title='Backache' color='red' />
-									<BadgeItem title='Bloating' color='red' />
-									<BadgeItem title='Body Pain' color='red' />
-									<BadgeItem title='Brain Issues' color='red' />
-									<BadgeItem title='Bloating' color='red' />
-									<BadgeItem title='COPD' color='red' />
+								<div className='col mb-4'>
+									{diseases_col_1?.map((item, index) => (
+										<BadgeItem key={index} title={item?.text} color='red' />
+									))}
 								</div>
-								<div className='col-3'>
-									<BadgeItem title='Chronic Kidney Disease' color='green' />
-									<BadgeItem title='Constipation' color='green' />
-									<BadgeItem title='Depression' color='green' />
-									<BadgeItem title='Diabetes' color='green' />
-									<BadgeItem title='Eczema' color='green' />
-									<BadgeItem title='Epilepsy' color='green' />
-									<BadgeItem title='Fatty Liver' color='green' />
-									<BadgeItem title='Fibromyalgia' color='green' />
-									<BadgeItem title='Frozen Shoulder' color='green' />
-									<BadgeItem title='Gastritis' color='green' />
-									<BadgeItem title='Gout' color='green' />
-									<BadgeItem title='Hairfall' color='green' />
-									<BadgeItem title='Headache' color='green' />
+								<div className='col mb-4'>
+									{diseases_col_2?.map((item, index) => (
+										<BadgeItem key={index} title={item?.text} color='green' />
+									))}
 								</div>
-								<div className='col-3'>
-									<BadgeItem title='Acid Reflux' color='yellow' />
-									<BadgeItem title='Acidity' color='yellow' />
-									<BadgeItem title='Acne' color='yellow' />
-									<BadgeItem title='Alzheimer' color='yellow' />
-									<BadgeItem title='Ankylosing Spondylitis' color='yellow' />
-									<BadgeItem title='Anxiety' color='yellow' />
-									<BadgeItem title='Arthritis' color='yellow' />
-									<BadgeItem title='Asthma' color='yellow' />
-									<BadgeItem title='Backache' color='yellow' />
-									<BadgeItem title='Bloating' color='yellow' />
-									<BadgeItem title='Body Pain' color='yellow' />
-									<BadgeItem title='Brain Issues' color='yellow' />
-									<BadgeItem title='Bloating' color='yellow' />
-									<BadgeItem title='COPD' color='yellow' />
+								<div className='col mb-4'>
+									{diseases_col_3?.map((item, index) => (
+										<BadgeItem key={index} title={item?.text} color='yellow' />
+									))}
 								</div>
-								<div className='col-3'>
-									<BadgeItem title='Acid Reflux' color='sky' />
-									<BadgeItem title='Acidity' color='sky' />
-									<BadgeItem title='Acne' color='sky' />
-									<BadgeItem title='Alzheimer' color='sky' />
-									<BadgeItem title='Ankylosing Spondylitis' color='sky' />
-									<BadgeItem title='Anxiety' color='sky' />
-									<BadgeItem title='Arthritis' color='sky' />
-									<BadgeItem title='Asthma' color='sky' />
-									<BadgeItem title='Backache' color='sky' />
-									<BadgeItem title='Bloating' color='sky' />
-									<BadgeItem title='Body Pain' color='sky' />
-									<BadgeItem title='Brain Issues' color='sky' />
-									<BadgeItem title='Bloating' color='sky' />
-									<BadgeItem title='COPD' color='sky' />
+								<div className='col mb-4'>
+									{diseases_col_4?.map((item, index) => (
+										<BadgeItem key={index} title={item?.text} color='sky' />
+									))}
+								</div>
+								<div className='col mb-4'>
+									{diseases_col_5?.map((item, index) => (
+										<BadgeItem key={index} title={item?.text} color='purple' />
+									))}
 								</div>
 							</div>
 						</div>
 					</div>
 				)}
-				<div className='text-center mt-5'>
-					<Link href='/services	'>
-						<a className='btn btn-outline-primary mt-2'>
-							View All Reversal Programs
-						</a>
-					</Link>
-				</div>
+				{!diseases_col_1 && (
+					<div className='text-center mt-5'>
+						<Link href='/services	'>
+							<a className='btn btn-outline-primary mt-2'>
+								View All Reversal Programs
+							</a>
+						</Link>
+					</div>
+				)}
 			</div>
 		</section>
 	);
