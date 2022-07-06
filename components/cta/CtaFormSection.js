ddb73f527c6885137202a50ec5e06959ac5c6260
swarnab99 <swarnab93@gmail.com>
2022-07-06 18:30:47 +0530

    cta contact form section added

--- components/cta/CtaFormSection.js
+++ components/cta/CtaFormSection.js
@@ -1,9 +1,72 @@
-/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
+import { useState } from 'react';
+import Airtable from 'airtable';
+
+const base = new Airtable({
+	apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
 
 const CtaFormSection = ({ slice }) => {
 	// console.log(slice);
 	const { heading, description } = slice?.primary;
+
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		phone: '',
+		city: '',
+		message: '',
+	});
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState(false);
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+		setError(null);
+		setSuccess(false);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			const data = await base('Appointment Queries').create([
+				{
+					fields: {
+						Name: formData.name,
+						Email: formData.email,
+						Phone: formData.phone,
+						City: formData.city,
+						Message: formData.message,
+
+						Source: location.href,
+						Status: 'Todo',
+					},
+				},
+			]);
+
+			// console.log(data);
+
+			setFormData({
+				name: '',
+				email: '',
+				phone: '',
+				city: '',
+				message: '',
+			});
+			setSuccess(true);
+			setLoading(false);
+		} catch (error) {
+			setError(error);
+			console.log(error);
+			setLoading(false);
+		}
+	};
+
 	return (
 		<section className='wrapper bg-light'>
 			<div className='container pt-14 pt-md-16'>
@@ -33,56 +96,49 @@ const CtaFormSection = ({ slice }) => {
 							<div className='col-lg-6'>
 								<form
 									className='contact-form needs-validation'
-									method='post'
-									action='/assets/php/contact.php'>
+									onSubmit={handleSubmit}>
 									<div className='messages'></div>
 									<div className='row gx-4'>
 										<div className='col-md-6'>
 											<div className='form-floating mb-4'>
 												<input
-													id='frm_name'
 													type='text'
 													name='name'
-													className='form-control border-0'
-													placeholder='Jane'
-													required='required'
+													value={formData.name}
+													onChange={handleChange}
+													placeholder='Name'
+													className='form-control'
+													required
 												/>
 												<label htmlFor='frm_name'>Name *</label>
-												<div className='invalid-feedback'>
-													Please enter your name.
-												</div>
 											</div>
 										</div>
 
 										<div className='col-md-6'>
 											<div className='form-floating mb-4'>
 												<input
-													id='frm_email'
 													type='email'
 													name='email'
-													className='form-control border-0'
-													placeholder='jane.doe@example.com'
-													required='required'
-													data-error='Valid email is required.'
+													value={formData.email}
+													onChange={handleChange}
+													className='form-control'
+													placeholder='Email'
+													required
 												/>
 												<label htmlFor='frm_email'>Email *</label>
-												<div className='valid-feedback'>Looks good!</div>
-												<div className='invalid-feedback'>
-													Please provide a valid email address.
-												</div>
 											</div>
 										</div>
 
 										<div className='col-md-6'>
 											<div className='form-floating mb-4'>
 												<input
-													id='phone'
 													type='tel'
 													name='phone'
-													className='form-control border-0'
-													placeholder='jane.doe@example.com'
-													required='required'
-													data-error='Valid phone is required.'
+													value={formData.phone}
+													onChange={handleChange}
+													className='form-control'
+													placeholder='Phone'
+													required
 												/>
 												<label htmlFor='frm_email'>Phone *</label>
 											</div>
@@ -90,13 +146,13 @@ const CtaFormSection = ({ slice }) => {
 										<div className='col-md-6'>
 											<div className='form-floating mb-4'>
 												<input
-													id='city'
-													type='tel'
+													type='text'
 													name='city'
-													className='form-control border-0'
-													placeholder='jane.doe@example.com'
-													required='required'
-													data-error='Valid city is required.'
+													value={formData.city}
+													onChange={handleChange}
+													className='form-control'
+													placeholder='City'
+													required
 												/>
 												<label htmlFor='frm_email'>City *</label>
 											</div>
@@ -105,29 +161,47 @@ const CtaFormSection = ({ slice }) => {
 										<div className='col-12'>
 											<div className='form-floating mb-4'>
 												<textarea
-													id='frm_message'
 													name='message'
-													className='form-control border-0'
-													placeholder='Your message'
+													value={formData.message}
+													onChange={handleChange}
+													rows='3'
+													className='form-control'
+													placeholder='Type your message here...'
 													style={{ height: '150px' }}
 													required></textarea>
 												<label htmlFor='frm_message'>Message *</label>
-												<div className='valid-feedback'>Looks good!</div>
-												<div className='invalid-feedback'>
-													Please enter your messsage.
-												</div>
 											</div>
 										</div>
 
 										<div className='col-12'>
-											<input
+											<button
 												type='submit'
 												className='btn btn-outline-primary btn-send mb-3'
-												value='Send message'
-											/>
+												value='Send message'>
+												{loading ? 'Sending...' : 'Submit'}
+											</button>
 										</div>
 									</div>
 								</form>
+								<footer
+									className={`notification-box mb-50 ${
+										success ? 'show-success' : error ? 'show-error' : ''
+									}`}>
+									{error && (
+										<div className='col-lg-12 col-md-12 col-sm-12 mb-4'>
+											<p className='text-danger text-center h2'>
+												{error.message}
+											</p>
+										</div>
+									)}
+									{success && (
+										<div className='col-lg-12 col-md-12 col-sm-12 mb-4'>
+											<p className='text-success text-center h2'>
+												Thanks, we will contact you soon.
+											</p>
+										</div>
+									)}
+								</footer>
 							</div>
 						</div>
 					</div>
