33dd0ffc8b9711c846d3497dcb37bd1fae117812
Darpan Chakraborty <favfly.darpan@gmail.com>
2023-04-29 11:10:00 +0530

    update

--- components/blog/BlogPostSection.js
+++ components/blog/BlogPostSection.js
@@ -1,297 +1,297 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
-import { FiEye, FiUser, FiCalendar } from 'react-icons/fi';
-import { useState, useEffect } from 'react';
-import { RichText } from 'prismic-reactjs';
-import Airtable from 'airtable';
-import { linkResolver } from '../../prismic-configuration';
-import dayjs from 'dayjs';
+import { FiEye, FiUser, FiCalendar } from "react-icons/fi";
+import { useState, useEffect } from "react";
+import { RichText } from "prismic-reactjs";
+import Airtable from "airtable";
+import { linkResolver } from "../../prismic-configuration";
+import dayjs from "dayjs";
 dayjs().format();
-import countapi from 'countapi-js';
+import countapi from "countapi-js";
 import {
-	EmailShareButton,
-	FacebookShareButton,
-	LinkedinShareButton,
-	PinterestShareButton,
-	TwitterShareButton,
-	WhatsappShareButton,
-	FacebookIcon,
-	EmailIcon,
-	LinkedinIcon,
-	TwitterIcon,
-	WhatsappIcon,
-	PinterestIcon,
-} from 'react-share';
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  PinterestShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  EmailIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  PinterestIcon,
+} from "react-share";
 
 const base = new Airtable({
-	apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
 }).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
 
 const BlogPostSection = ({ blogPost }) => {
-	// console.log(blogPost);
-	const uid = blogPost?.uid;
-	const { title, description, featured_image, published_date, body } =
-		blogPost?.data;
+  // console.log(blogPost);
+  const uid = blogPost?.uid;
+  const { title, description, featured_image, published_date, body } =
+    blogPost?.data;
 
-	/* ===== COUNT & UPDATE NO. OF VIEWS ===== */
-	const [views, setViews] = useState(0);
-	useEffect(() => {
-		countapi.hit(`${process.env.PRISMIC_ID}.in`, uid).then((result) => {
-			result?.value && setViews(result.value);
-		});
-	}, [uid]);
-	/* ===== END ===== */
+  /* ===== COUNT & UPDATE NO. OF VIEWS ===== */
+  // const [views, setViews] = useState(0);
+  // useEffect(() => {
+  // 	countapi.hit(`${process.env.PRISMIC_ID}.in`, uid).then((result) => {
+  // 		result?.value && setViews(result.value);
+  // 	});
+  // }, [uid]);
+  /* ===== END ===== */
 
-	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		comment: '',
-	});
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
-	const [success, setSuccess] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    comment: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
-	const handleChange = (e) => {
-		setFormData({
-			...formData,
-			[e.target.name]: e.target.value,
-		});
-		setError(null);
-		setSuccess(false);
-	};
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    setError(null);
+    setSuccess(false);
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setLoading(true);
-		try {
-			const data = await base('Blog Queries').create([
-				{
-					fields: {
-						Name: formData.name,
-						Email: formData.email,
-						Comment: formData.comment,
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const data = await base("Blog Queries").create([
+        {
+          fields: {
+            Name: formData.name,
+            Email: formData.email,
+            Comment: formData.comment,
 
-						Source: location.href,
-						Status: 'Todo',
-					},
-				},
-			]);
+            Source: location.href,
+            Status: "Todo",
+          },
+        },
+      ]);
 
-			// console.log(data);
+      // console.log(data);
 
-			setFormData({
-				name: '',
-				email: '',
-				comment: '',
-			});
-			setSuccess(true);
-			setLoading(false);
-		} catch (error) {
-			setError(error);
-			console.log(error);
-			setLoading(false);
-		}
-	};
-	return (
-		<>
-			<section className='wrapper bg-soft-primary'>
-				<div className='container pt-10 pb-19 pt-md-14 pb-md-20 text-center'>
-					<div className='row'>
-						<div className='col-md-10 col-xl-8 mx-auto'>
-							<div className='post-header'>
-								{/* <div className='post-category text-line'>
+      setFormData({
+        name: "",
+        email: "",
+        comment: "",
+      });
+      setSuccess(true);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <section className="wrapper bg-soft-primary">
+        <div className="container pt-10 pb-19 pt-md-14 pb-md-20 text-center">
+          <div className="row">
+            <div className="col-md-10 col-xl-8 mx-auto">
+              <div className="post-header">
+                {/* <div className='post-category text-line'>
 									<a href='#' className='hover' rel='category'>
 										NUTRITION
 									</a>
 								</div> */}
 
-								<h1 className='display-1 mt-8 mb-3'>{title[0]?.text}</h1>
-								<p className='mb-3'>{description[0]?.text}</p>
-								<ul className='post-meta mb-5'>
-									<li className='post-date'>
-										<i className=''>
-											<FiCalendar />
-										</i>
-										<span> {dayjs(published_date).format('DD MMM, YYYY')}</span>
-									</li>
-									<li className='post-author'>
-										<i className=''>
-											<FiUser />
-										</i>
-										<span>By Admin</span>
-									</li>
-									<li className='post-likes'>
-										<i className=''>
-											<FiEye />
-										</i>
-										{views}
-										<span> Views</span>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+                <h1 className="display-1 mt-8 mb-3">{title[0]?.text}</h1>
+                <p className="mb-3">{description[0]?.text}</p>
+                <ul className="post-meta mb-5">
+                  <li className="post-date">
+                    <i className="">
+                      <FiCalendar />
+                    </i>
+                    <span> {dayjs(published_date).format("DD MMM, YYYY")}</span>
+                  </li>
+                  <li className="post-author">
+                    <i className="">
+                      <FiUser />
+                    </i>
+                    <span>By Admin</span>
+                  </li>
+                  {/* <li className="post-likes">
+                    <i className="">
+                      <FiEye />
+                    </i>
+                    {views}
+                    <span> Views</span>
+                  </li> */}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-			<section className='wrapper bg-light'>
-				<div className='container pb-14 pb-md-16'>
-					<div className='row'>
-						<div className='col-lg-10 mx-auto p-0'>
-							<div className='blog single mt-n17'>
-								<div className='card'>
-									<figure className='card-img-top'>
-										<img src={featured_image?.url} alt={featured_image?.alt} />
-									</figure>
-									<div className='card-body p-2 px-lg-12 py-lg-10'>
-										<div className='classic-view'>
-											<article className='post'>
-												<div className='post-content'>
-													{body &&
-														body.map((item, index) => (
-															<DetailsBox key={index} postBlock={item} />
-														))}
-												</div>
-												<div className='post-footer d-md-flex flex-md-row justify-content-md-between align-items-center mt-8'>
-													Share the article:
-													<div className='mt-2 mt-md-0'>
-														<FacebookShareButton
-															className='me-2'
-															url={`https://reversefactor.in/${uid}`}>
-															<FacebookIcon size={32} round={true} />
-														</FacebookShareButton>
-														<EmailShareButton
-															className='me-2'
-															url={`https://reversefactor.in/${uid}`}>
-															<EmailIcon size={32} round={true} />
-														</EmailShareButton>
-														<LinkedinShareButton
-															className='me-2'
-															url={`https://reversefactor.in/${uid}`}>
-															<LinkedinIcon size={32} round={true} />
-														</LinkedinShareButton>
-														<TwitterShareButton
-															className='me-2'
-															url={`https://reversefactor.in/${uid}`}>
-															<TwitterIcon size={32} round={true} />
-														</TwitterShareButton>
-														<PinterestShareButton
-															className='me-2'
-															url={`https://reversefactor.in/${uid}`}>
-															<PinterestIcon size={32} round={true} />
-														</PinterestShareButton>
-														<WhatsappShareButton
-															url={`https://reversefactor.in/${uid}`}>
-															<WhatsappIcon size={32} round={true} />
-														</WhatsappShareButton>
-													</div>
-												</div>
-											</article>
-										</div>
+      <section className="wrapper bg-light">
+        <div className="container pb-14 pb-md-16">
+          <div className="row">
+            <div className="col-lg-10 mx-auto p-0">
+              <div className="blog single mt-n17">
+                <div className="card">
+                  <figure className="card-img-top">
+                    <img src={featured_image?.url} alt={featured_image?.alt} />
+                  </figure>
+                  <div className="card-body p-2 px-lg-12 py-lg-10">
+                    <div className="classic-view">
+                      <article className="post">
+                        <div className="post-content">
+                          {body &&
+                            body.map((item, index) => (
+                              <DetailsBox key={index} postBlock={item} />
+                            ))}
+                        </div>
+                        <div className="post-footer d-md-flex flex-md-row justify-content-md-between align-items-center mt-8">
+                          Share the article:
+                          <div className="mt-2 mt-md-0">
+                            <FacebookShareButton
+                              className="me-2"
+                              url={`https://reversefactor.in/${uid}`}>
+                              <FacebookIcon size={32} round={true} />
+                            </FacebookShareButton>
+                            <EmailShareButton
+                              className="me-2"
+                              url={`https://reversefactor.in/${uid}`}>
+                              <EmailIcon size={32} round={true} />
+                            </EmailShareButton>
+                            <LinkedinShareButton
+                              className="me-2"
+                              url={`https://reversefactor.in/${uid}`}>
+                              <LinkedinIcon size={32} round={true} />
+                            </LinkedinShareButton>
+                            <TwitterShareButton
+                              className="me-2"
+                              url={`https://reversefactor.in/${uid}`}>
+                              <TwitterIcon size={32} round={true} />
+                            </TwitterShareButton>
+                            <PinterestShareButton
+                              className="me-2"
+                              url={`https://reversefactor.in/${uid}`}>
+                              <PinterestIcon size={32} round={true} />
+                            </PinterestShareButton>
+                            <WhatsappShareButton
+                              url={`https://reversefactor.in/${uid}`}>
+                              <WhatsappIcon size={32} round={true} />
+                            </WhatsappShareButton>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
 
-										<hr />
-										<h3 className='mb-3'>
-											Would you like to share your thoughts?
-										</h3>
-										<p className='mb-7'>
-											We won't spam you. Required fields are marked *
-										</p>
-										<form onSubmit={handleSubmit} className='comment-form'>
-											<div className='form-floating mb-4'>
-												<input
-													type='text'
-													name='name'
-													value={formData.name}
-													onChange={handleChange}
-													placeholder='Name'
-													className='form-control'
-													required
-												/>
-												<label htmlFor='c-name'>Name *</label>
-											</div>
-											<div className='form-floating mb-4'>
-												<input
-													type='email'
-													name='email'
-													value={formData.email}
-													onChange={handleChange}
-													className='form-control'
-													placeholder='Email'
-													required
-												/>
-												<label htmlFor='c-email'>Email*</label>
-											</div>
-											<div className='form-floating mb-4'>
-												<textarea
-													name='comment'
-													value={formData.comment}
-													onChange={handleChange}
-													rows='3'
-													className='form-control'
-													placeholder='Type your comment here...'
-													required
-													style={{ height: '150px' }}></textarea>
-												<label>Comment *</label>
-											</div>
-											<button
-												type='submit'
-												className='btn btn-primary rounded-pill mb-0'>
-												{loading ? 'Sending...' : 'Submit'}
-											</button>
-										</form>
-									</div>
-									<footer
-										className={`notification-box mb-50 ${
-											success ? 'show-success' : error ? 'show-error' : ''
-										}`}>
-										{error && (
-											<div className='col-lg-12 col-md-12 col-sm-12 mb-4'>
-												<p className='text-danger text-center h2'>
-													{error.message}
-												</p>
-											</div>
-										)}
-										{success && (
-											<div className='col-lg-12 col-md-12 col-sm-12 mb-4'>
-												<p className='text-success text-center h2'>
-													Thanks, for sharing your valuable thoughts.
-												</p>
-											</div>
-										)}
-									</footer>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+                    <hr />
+                    <h3 className="mb-3">
+                      Would you like to share your thoughts?
+                    </h3>
+                    <p className="mb-7">
+                      We won't spam you. Required fields are marked *
+                    </p>
+                    <form onSubmit={handleSubmit} className="comment-form">
+                      <div className="form-floating mb-4">
+                        <input
+                          type="text"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleChange}
+                          placeholder="Name"
+                          className="form-control"
+                          required
+                        />
+                        <label htmlFor="c-name">Name *</label>
+                      </div>
+                      <div className="form-floating mb-4">
+                        <input
+                          type="email"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                          className="form-control"
+                          placeholder="Email"
+                          required
+                        />
+                        <label htmlFor="c-email">Email*</label>
+                      </div>
+                      <div className="form-floating mb-4">
+                        <textarea
+                          name="comment"
+                          value={formData.comment}
+                          onChange={handleChange}
+                          rows="3"
+                          className="form-control"
+                          placeholder="Type your comment here..."
+                          required
+                          style={{ height: "150px" }}></textarea>
+                        <label>Comment *</label>
+                      </div>
+                      <button
+                        type="submit"
+                        className="btn btn-primary rounded-pill mb-0">
+                        {loading ? "Sending..." : "Submit"}
+                      </button>
+                    </form>
+                  </div>
+                  <footer
+                    className={`notification-box mb-50 ${
+                      success ? "show-success" : error ? "show-error" : ""
+                    }`}>
+                    {error && (
+                      <div className="col-lg-12 col-md-12 col-sm-12 mb-4">
+                        <p className="text-danger text-center h2">
+                          {error.message}
+                        </p>
+                      </div>
+                    )}
+                    {success && (
+                      <div className="col-lg-12 col-md-12 col-sm-12 mb-4">
+                        <p className="text-success text-center h2">
+                          Thanks, for sharing your valuable thoughts.
+                        </p>
+                      </div>
+                    )}
+                  </footer>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 const DetailsBox = ({ postBlock }) => {
-	// console.log(postBlock);
-	return (
-		<div className='post-block mx-auto p-0'>
-			<RichText
-				render={postBlock?.primary?.details}
-				linkResolver={linkResolver}
-			/>
+  // console.log(postBlock);
+  return (
+    <div className="post-block mx-auto p-0">
+      <RichText
+        render={postBlock?.primary?.details}
+        linkResolver={linkResolver}
+      />
 
-			<div className='mb-5 row'>
-				{postBlock?.items &&
-					postBlock?.items.map((item, index) => (
-						<div key={index} className='mb-4 col-md-6'>
-							<img
-								className='img-fluid lozad'
-								data-src={item?.image?.url}
-								alt={item?.image?.alt}
-							/>
-						</div>
-					))}
-			</div>
-		</div>
-	);
+      <div className="mb-5 row">
+        {postBlock?.items &&
+          postBlock?.items.map((item, index) => (
+            <div key={index} className="mb-4 col-md-6">
+              <img
+                className="img-fluid lozad"
+                data-src={item?.image?.url}
+                alt={item?.image?.alt}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogPostSection;
--- components/blog/BlogsSection.js
+++ components/blog/BlogsSection.js
@@ -1,84 +1,84 @@
 /* eslint-disable @next/next/no-img-element */
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import dayjs from 'dayjs';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import dayjs from "dayjs";
 dayjs().format();
-import countapi from 'countapi-js';
-import { FiCalendar, FiEye } from 'react-icons/fi';
+import countapi from "countapi-js";
+import { FiCalendar, FiEye } from "react-icons/fi";
 
 const BlogsSection = ({ blogPosts }) => {
-	// console.log(blogPosts);
-	return (
-		<section className='wrapper mt-14 bg-light'>
-			<div className='container'>
-				<h2 className='display-4 mb-10 text-center'>Recent Blog Posts</h2>
-				{/* <p className='lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0'>
+  // console.log(blogPosts);
+  return (
+    <section className="wrapper mt-14 bg-light">
+      <div className="container">
+        <h2 className="display-4 mb-10 text-center">Recent Blog Posts</h2>
+        {/* <p className='lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0'>
 					Here are the latest tips from our blog that got the most attention.
 				</p> */}
-				<div className='row'>
-					{blogPosts?.data?.allBlog_posts?.edges?.map((item, index) => (
-						<BlogItem key={index} data={item} />
-					))}
-				</div>
-			</div>
-		</section>
-	);
+        <div className="row">
+          {blogPosts?.data?.allBlog_posts?.edges?.map((item, index) => (
+            <BlogItem key={index} data={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 const BlogItem = ({ data }) => {
-	const { title, featured_image, published_date, _meta } = data.node;
-	/* ===== COUNT & UPDATE NO. OF VIEWS ===== */
-	const [views, setViews] = useState(0);
-	useEffect(() => {
-		countapi.get(`${process.env.PRISMIC_ID}.in`, _meta?.uid).then((result) => {
-			result?.value && setViews(result.value);
-		});
-	}, [_meta?.uid]);
-	/* ===== END ===== */
-	return (
-		<div className='col-lg-4 mb-10'>
-			<article>
-				<figure className='overlay overlay-1 hover-scale rounded mb-5'>
-					<Link href={`/${_meta?.uid}`}>
-						<a>
-							<img
-								src={featured_image?.custom?.url}
-								alt={featured_image?.alt}
-								className='img-fluid lozad'
-							/>
-						</a>
-					</Link>
-					<figcaption>
-						<h5 className='from-top mb-0'>Read More</h5>
-					</figcaption>
-				</figure>
-				<div className='post-header'>
-					<h2 className='post-title h3 mt-1 mb-3'>
-						<Link href={`/${_meta?.uid}`}>
-							<a className='link-dark'>{title[0]?.text}</a>
-						</Link>
-					</h2>
-				</div>
+  const { title, featured_image, published_date, _meta } = data.node;
+  /* ===== COUNT & UPDATE NO. OF VIEWS ===== */
+  // const [views, setViews] = useState(0);
+  // useEffect(() => {
+  // 	countapi.get(`${process.env.PRISMIC_ID}.in`, _meta?.uid).then((result) => {
+  // 		result?.value && setViews(result.value);
+  // 	});
+  // }, [_meta?.uid]);
+  /* ===== END ===== */
+  return (
+    <div className="col-lg-4 mb-10">
+      <article>
+        <figure className="overlay overlay-1 hover-scale rounded mb-5">
+          <Link href={`/${_meta?.uid}`}>
+            <a>
+              <img
+                src={featured_image?.custom?.url}
+                alt={featured_image?.alt}
+                className="img-fluid lozad"
+              />
+            </a>
+          </Link>
+          <figcaption>
+            <h5 className="from-top mb-0">Read More</h5>
+          </figcaption>
+        </figure>
+        <div className="post-header">
+          <h2 className="post-title h3 mt-1 mb-3">
+            <Link href={`/${_meta?.uid}`}>
+              <a className="link-dark">{title[0]?.text}</a>
+            </Link>
+          </h2>
+        </div>
 
-				<div className='post-footer'>
-					<ul className='post-meta'>
-						<li className='post-date'>
-							<i className=''>
-								<FiCalendar />
-							</i>
-							{dayjs(published_date).format('DD MMM, YYYY')}
-						</li>
-						<li className='post-comments'>
-							<i className=''>
-								<FiEye />
-							</i>
-							{views}
-						</li>
-					</ul>
-				</div>
-			</article>
-		</div>
-	);
+        <div className="post-footer">
+          <ul className="post-meta">
+            <li className="post-date">
+              <i className="">
+                <FiCalendar />
+              </i>
+              {dayjs(published_date).format("DD MMM, YYYY")}
+            </li>
+            {/* <li className="post-comments">
+              <i className="">
+                <FiEye />
+              </i>
+              {views}
+            </li> */}
+          </ul>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default BlogsSection;
